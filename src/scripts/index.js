c7a88a4981d715a6c94ccab850d795039d63ebfc
ismiralrasyiid <ismiralrasyiid@gmail.com>
2023-06-01 11:23:59 +0700

    first commit

---
+++ src/scripts/index.js
@@ -0,0 +1,6 @@
+import 'regenerator-runtime'; /* for async await transpile */
+import '../styles/main.css';
+
+const rootContainer = document.getElementById('root');
+rootContainer.innerText = 'bisa nih bos';
+console.log('Hello Coders! :)');
